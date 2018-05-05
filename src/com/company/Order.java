f86c0e9d3965e8d617d9807b4620724db8eed43e
Danila Suslov <idenimail33@gmail.com>
2018-05-05 14:20:26 +0400

    commit

--- src/com/company/Order.java
+++ src/com/company/Order.java
@@ -4,49 +4,55 @@ public class Order {
     private int size;
     private Dish[] dishes;
 
-    public Order () {this ( 16);}
+    public Order() {
+        this(16);
+    }
 
-    public Order (int size){
+
+    public Order(int size) {
         dishes = new Dish[size];
         this.size = 0;
     }
-    public Order (Dish[] dishes){
+
+    public Order(Dish[] dishes) {
         this.dishes = dishes;
         this.size = 0;
     }
 
-    public boolean add (Dish dish){
+    public boolean add(Dish dish) {
         if (size >= dishes.length)
-        dishes[size] = dish;
+            dishes[size] = dish;
         size++;
         return true;
     }
-   public boolean remove(String dishName){
-       for (int i = 0; i < size; i++) {
 
-           if (dishes[i].getName().equals(dishName)) {
+    public boolean remove(String dishName) {
+        for (int i = 0; i < size; i++) {
 
-               for (int j = i; j < size; j++) {
-                   dishes[j] = dishes[j + 1];
+            if (dishes[i].getName().equals(dishName)) {
 
-                   if (j == size) {
-                       dishes[j + 1] = null;
-                   }
-               }
-               size--;
-               return true;
-           }
-       }
-       return false;
+                for (int j = i; j < size; j++) {
+                    dishes[j] = dishes[j + 1];
+
+                    if (j == size) {
+                        dishes[j + 1] = null;
+                    }
+                }
+                size--;
+                return true;
+            }
+        }
+        return false;
     }
 
-    public int removeAll (String dishName){
-    int count = 0;
-        while (remove(dishName)){
+    public int removeAll(String dishName) {
+        int count = 0;
+        while (remove(dishName)) {
             count++;
         }
         return count;
     }
+
     public int dishQuantity() {
         return size;
     }
@@ -61,11 +67,12 @@ public class Order {
         return count;
     }
 
-        public Dish[] getDishes() {
-            Dish[] dishef = new Dish[dishQuantity()];
-            System.arraycopy(dishes, 0, dishes, 0, dishes.length);
-            return dishes;
+    public Dish[] getDishes() {
+        Dish[] dishef = new Dish[dishQuantity()];
+        System.arraycopy(dishes, 0, dishes, 0, dishes.length);
+        return dishes;
     }
+
     public double costTotal() {
         double sum = 0;
         for (Dish dish : dishes) {
@@ -75,11 +82,12 @@ public class Order {
         }
         return sum;
     }
+
     public String[] dishesNames() {
         int h = 0;
         String[] dishNames = new String[size];
         for (int i = 0; i < size; i++) {
-            if (!prover(dishNames, dishes[i].getName())) {
+            if (!matches(dishNames, dishes[i].getName())) {
                 dishNames[h++] = dishes[i].getName();
             }
         }
@@ -89,28 +97,26 @@ public class Order {
     }
 
 
-    private boolean prover(String[] dishNames, String dishName) {
-        for (int i = 0; i < dishNames.length; i++) {
-            String dn = dishNames[i];
+    private boolean matches(String[] dishNames, String dishName) {
+        for (String dn : dishNames) {
             if (dn != null && dn.equals(dishName))
                 return true;
         }
         return false;
     }
-    public Dish [] sortedDishesByCostDesc() {
 
-        for (int i = 0; i <dishes.length; i++)
-        {
-            for (int j = 0; j < dishes.length - i - 1 ; j++)
-            {
-                if (dishes[j].getCost() < dishes[j + 1].getCost())
-                {
-                    Dish temp = dishes[j];
-                    dishes[j] = dishes[j + 1];
-                    dishes[j + 1] = temp;
+    public Dish[] sortedDishesByCostDesc() {
+        Dish[] dishes1 = this.dishes;
+        for (int i = 0; i < dishes1.length; i++) {
+            for (int j = 0; j < dishes1.length - i - 1; j++) {
+                if (dishes1[j].getCost() < dishes1[j + 1].getCost()) {
+                    Dish temp = dishes1[j];
+                    dishes1[j] = dishes1[j + 1];
+                    dishes1[j + 1] = temp;
                 }
             }
         }
+        return dishes1;
 
     }
 }
